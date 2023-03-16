88ef6c5a5722ded0583d04092e749f4f9866c46e
Frishiits <122269330+Frishiits@users.noreply.github.com>
2023-03-16 14:02:56 +0200

    tiek iesākts darbs

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar">
+  <ul>
+    <li><a href="#">Sākums</a></li>
+    <li><a href="#">Kalendārs</a></li>
+    <li><a href="#">Informācija</a></li>
+</nav>
+
+
+    
+<footer>
+  <div class="container">
+    <p>Liepājas Valsts tehnikums &copy 2023</p>
+  </div>
+</footer>
+</body>
+</html>
