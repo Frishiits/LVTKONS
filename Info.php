95cce3b241afa4ed6b662aa1217668ab6077f86e
DrErrorWork <daniss404@gmx.com>
2023-03-20 11:59:32 +0200

    changes to design and added few php functions

--- Info.php
+++ Info.php
@@ -25,7 +25,8 @@
     <meta property="og:type" content="website">
   <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
   <body class="u-body u-xl-mode" data-lang="en">
-  <?php 
+<?php 
+session_start();
 include  ('header.php');
 ?>
     <section class="u-clearfix u-grey-10 u-section-1" id="sec-49c2">
@@ -43,9 +44,10 @@ include  ('header.php');
               </div>
               <div class="u-align-left u-container-style u-layout-cell u-right-cell u-shape-rectangle u-size-37 u-layout-cell-2">
                 <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xl u-valign-top-xs u-container-layout-2">
-                  <h2 class="u-text u-text-default u-text-1">$Vards $Uzvards</h2>
+                  <h2 class="u-text u-text-default u-text-1"><?=$_SESSION['fullname']; ?></h2>
                   <p class="u-text u-text-2"> TEXT TEXTTEXT TEXTTEXT TEXTTEXT TEXTTEXT TEXTTEXT TEXT</p>
-                  <a href="" class="u-btn u-button-style u-btn-1">Button</a>
+                  <!--<a href="" class="u-btn u-button-style u-btn-1">Button</a>
+                  !-->
                 </div>
               </div>
             </div>
