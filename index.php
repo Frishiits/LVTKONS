938973993877c4ed99de8e41785a67aa3bbf6647
DrErrorWork <daniss404@gmx.com>
2023-03-21 14:16:41 +0200

    alerts if you going to a consultation

--- index.php
+++ index.php
@@ -12,9 +12,12 @@
   <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
   <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
   <meta name="generator" content="Nicepage 5.6.13, nicepage.com">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  
   <link id="u-theme-google-font" rel="stylesheet"
     href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
-
+<link rel="stylesheet" href="css/overwrite.css" media="screen">
   <meta name="theme-color" content="#478ac9">
   <meta property="og:title" content="Home">
   <meta property="og:type" content="website">
@@ -33,11 +36,31 @@
           <?= substr($_SESSION['username'], 0, -1); ?>!
         </h2>
         <?php
+          echo '<div class="same-line">';
+          echo '<div class="alert alert-warning" role="alert">';
+          echo ' <p class="u-text u-align-left" ><span class="u-file-icon u-icon"><img
+          src="images/3652191.png" alt=""></span>&nbspNedēļas pārskats: <br> EU KAS NOTIEK?</p>';
+          //echo implode(', ', $errors);
+          echo '</div>';
+
+          echo '<div class="alert alert-warning" role="alert">';
+          echo ' <p class="u-text u-align-left" ><span class="u-file-icon u-icon"><img
+          src="images/3652191.png" alt=""></span>&nbspNedēļas pārskats: <br> EU KAS NOTIEK?</p>';
+          //echo implode(', ', $errors);
+          echo '</div>';
+
+
+          echo '<div class="alert alert-warning" role="alert">';
+          echo ' <p class="u-text u-align-left" ><span class="u-file-icon u-icon"><img
+          src="images/3652191.png" alt=""></span>&nbspNedēļas pārskats: <br> EU KAS NOTIEK?</p>';
+          //echo implode(', ', $errors);
+          echo '</div>';
+
+          
+          echo '</div>';
         
         ?>
-        <p class="u-text u-text-2"><span class="u-file-icon u-icon"><img src="images/3652191.png"
-              alt=""></span>&nbspNedēļas pārskats:</p>
-
+       
         <br>
         <div class="u-repeater u-repeater-1">
           <div
