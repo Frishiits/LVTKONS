cab85845bea9d5a04906d1d8d1244b75fd4ef748
DrError <daniss404@gmx.com>
2023-03-23 18:43:54 +0100

    new php file

--- Konsultacija.php
+++ Konsultacija.php
@@ -6,88 +6,81 @@
     <meta name="description" content="">
     <title>Konsultacija</title>
     <link rel="stylesheet" href="nicepage.css" media="screen">
-<link rel="stylesheet" href="Konsultacija.css" media="screen">
+    <link rel="stylesheet" href="Konsultacija.css" media="screen">
     <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
     <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
     <meta name="generator" content="Nicepage 5.6.13, nicepage.com">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
-    
-    
-    <script type="application/ld+json">{
-		"@context": "http://schema.org",
-		"@type": "Organization",
-		"name": "",
-		"logo": "images/logo_new_balts.png"
-}</script>
+
     <meta name="theme-color" content="#478ac9">
     <meta property="og:title" content="Konsultacija">
     <meta property="og:type" content="website">
   <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
-  <body class="u-body u-xl-mode" data-lang="en">   
-<?php 
-include  ('header.php');
-?>
-    <section class="u-clearfix u-section-1" id="sec-a7f5">
-    <div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
- 
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-
-      </tr>
-    </tbody>
-  </table>
-</div>
-    </section>  
+  <body class="u-body u-xl-mode" data-lang="en">
+  <?php
+  include('header.php');
+  ?>
+    <section class="u-align-center u-clearfix u-section-1" id="sec-b1bc">
+      <p>CARDS HERE</p>
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-table u-table-responsive u-table-1">
+          <table class="u-table-entity">
+            <thead class="u-align-left u-custom-font u-palette-1-base u-table-header u-text-font u-table-header-1">
+              <tr style="height: 56px;">
+                <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-1">Vards Uzvārds</th>
+                <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-2">Pirmdiena</th>
+                <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-3">Otrdiena</th>
+                <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-4">Trešdiena</th>
+                <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-5">Ceturtdiena</th>
+              </tr>
+            </thead>
+            <tbody class="u-table-alt-grey-5 u-table-body u-white u-table-body-1">
+              <tr style="height: 51px;">
+                <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 1</td>
+                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-7"></td>
+                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-8"></td>
+                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-9"></td>
+                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-10">16.10 – 17.10&nbsp;<br>Ve A-105&nbsp;
+                </td>
+              </tr>
+              <tr style="height: 51px;">
+                <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 2</td>
+                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-12">16.10 – 17.10&nbsp;<br>Ve A-105&nbsp;
+                </td>
+                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+              </tr>
+              <tr style="height: 51px;">
+                <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 3</td>
+                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-18">16.10 – 17.10&nbsp;<br>Ve A-105&nbsp;
+                </td>
+                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+              </tr>
+              <tr style="height: 52px;">
+                <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 4<br>
+                </td>
+                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-24">16.10 – 17.10&nbsp;<br>Ve A-105&nbsp;
+                </td>
+                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-section-2" id="sec-e338">
+      <div class="u-clearfix u-sheet u-sheet-1"></div>
+    </section>
+    <?php
+  include('footer.php');
+  ?>
     
-    <div class="table-responsive">
-
-    
-    <?php 
-include ('footer.php');
-?>
-
 
   
 </body></html>
