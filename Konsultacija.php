1b9e18febd13ab9fd9f1855bd0636eb6da9ecc36
DrErrorWork <daniss404@gmx.com>
2023-03-29 11:34:51 +0300

    adding mobile phone view

--- Konsultacija.php
+++ Konsultacija.php
@@ -1,112 +1,123 @@
 <!DOCTYPE html>
-<html style="font-size: 16px;" lang="en"><head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <title>Konsultacija</title>
-    <link rel="stylesheet" href="nicepage.css" media="screen">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/overwrite.css" media="screen">
-    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
-    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
-    <meta name="generator" content="Nicepage 5.6.13, nicepage.com">
-    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+<html style="font-size: 16px;" lang="en">
 
-    <meta name="theme-color" content="#478ac9">
-    <meta property="og:title" content="Konsultacija">
-    <meta property="og:type" content="website">
-  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
-  <body class="u-body u-xl-mode" data-lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <title>Konsultacija</title>
+  <link rel="stylesheet" href="nicepage.css" media="screen">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/overwrite.css" media="screen">
+  <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+  <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+  <meta name="generator" content="Nicepage 5.6.13, nicepage.com">
+  <link id="u-theme-google-font" rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+
+  <meta name="theme-color" content="#478ac9">
+  <meta property="og:title" content="Konsultacija">
+  <meta property="og:type" content="website">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/">
+</head>
+
+<body class="u-body u-xl-mode" data-lang="en">
   <?php
   include('header.php');
   ?>
-    <section class="u-align-center u-clearfix u-section-1" id="sec-b1bc">
-      <div class="u-clearfix u-sheet u-sheet-1">
+  <section class="u-align-center u-clearfix u-section-1" id="sec-b1bc">
+    <div class="u-clearfix u-sheet u-sheet-1">
       <form>
-      <input type="text" placeholder="Search..." id="search-bar" />
-      <button type="submit" id="search-button">Search</button>
-    </form>
+        <input type="text" placeholder="Search..." id="search-bar" />
+        <button type="submit" id="search-button">Search</button>
+      </form>
       <div class="card-line">
-    <a href="#">
-      <div class="card">
-        <h2>Pirmdiena</h2>
+        <a href="#">
+          <div class="card">
+            <h2>Pirmdiena</h2>
+          </div>
+        </a>
+        <a href="#">
+          <div class="card">
+            <h2>Otrdiena</h2>
+          </div>
+        </a>
+        <a href="#">
+          <div class="card">
+            <h2>Trešdiena</h2>
+          </div>
+        </a>
+        <a href="#">
+          <div class="card">
+            <h2>Ceturtdiena</h2>
+          </div>
+        </a>
       </div>
-    </a>
-    <a href="#">
-      <div class="card">
-        <h2>Otrdiena</h2>
+      <div class="u-table u-table-responsive u-table-1">
+        <table class="u-table-entity mobile-hide">
+          <thead class="u-align-left u-custom-font u-palette-1-base u-table-header u-text-font u-table-header-1">
+            <tr style="height: 56px;">
+              <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-1">Vards Uzvārds</th>
+              <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-2">Pirmdiena</th>
+              <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-3">Otrdiena</th>
+              <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-4">Trešdiena</th>
+              <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-5">Ceturtdiena</th>
+            </tr>
+          </thead>
+          <tbody class="u-table-alt-grey-5 u-table-body u-white u-table-body-1">
+            <tr style="height: 51px;">
+              <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 1</td>
+              <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-7"></td>
+              <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-8"></td>
+              <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-9"></td>
+              <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-10">16.10 –
+                17.10&nbsp;<br>Ve A-105&nbsp;
+              </td>
+            </tr>
+            <tr style="height: 51px;">
+              <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 2</td>
+              <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-12">16.10 –
+                17.10&nbsp;<br>Ve A-105&nbsp;
+              </td>
+              <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+              <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+              <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+            </tr>
+            <tr style="height: 51px;">
+              <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 3</td>
+              <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+              <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-18">16.10 –
+                17.10&nbsp;<br>Ve A-105&nbsp;
+              </td>
+              <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+              <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+            </tr>
+            <tr style="height: 52px;">
+              <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 4<br>
+              </td>
+              <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+              <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+              <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-24">16.10 –
+                17.10&nbsp;<br>Ve A-105&nbsp;
+              </td>
+              <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    </a>
-    <a href="#">
-      <div class="card">
-        <h2>Trešdiena</h2>
-      </div>
-    </a>
-    <a href="#">
-      <div class="card">
-        <h2>Ceturtdiena</h2>
-      </div>
-    </a>
-</div>
-        <div class="u-table u-table-responsive u-table-1">
-          <table class="u-table-entity">
-            <thead class="u-align-left u-custom-font u-palette-1-base u-table-header u-text-font u-table-header-1">
-              <tr style="height: 56px;">
-                <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-1">Vards Uzvārds</th>
-                <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-2">Pirmdiena</th>
-                <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-3">Otrdiena</th>
-                <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-4">Trešdiena</th>
-                <th class="u-align-center u-border-3 u-border-grey-dark-1 u-table-cell u-table-cell-5">Ceturtdiena</th>
-              </tr>
-            </thead>
-            <tbody class="u-table-alt-grey-5 u-table-body u-white u-table-body-1">
-              <tr style="height: 51px;">
-                <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 1</td>
-                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-7"></td>
-                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-8"></td>
-                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-9"></td>
-                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-10">16.10 – 17.10&nbsp;<br>Ve A-105&nbsp;
-                </td>
-              </tr>
-              <tr style="height: 51px;">
-                <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 2</td>
-                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-12">16.10 – 17.10&nbsp;<br>Ve A-105&nbsp;
-                </td>
-                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
-                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
-                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
-              </tr>
-              <tr style="height: 51px;">
-                <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 3</td>
-                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
-                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-18">16.10 – 17.10&nbsp;<br>Ve A-105&nbsp;
-                </td>
-                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
-                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
-              </tr>
-              <tr style="height: 52px;">
-                <td class="u-border-1 u-border-grey-75 u-table-cell">Skolotajs 4<br>
-                </td>
-                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
-                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
-                <td class="u-align-center u-border-1 u-border-grey-75 u-table-cell u-table-cell-24">16.10 – 17.10&nbsp;<br>Ve A-105&nbsp;
-                </td>
-                <td class="u-border-1 u-border-grey-75 u-table-cell"></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </section>
-    <section class="u-clearfix u-section-2" id="sec-e338">
-      <div class="u-clearfix u-sheet u-sheet-1"></div>
-    </section>
-    <?php
+    </div>
+  </section>
+  <section class="u-clearfix u-section-2" id="sec-e338">
+    <div class="u-clearfix u-sheet u-sheet-1"></div>
+  </section>
+  <?php
   include('footer.php');
   ?>
-    
 
-  
-</body></html>
+
+
+</body>
+
+</html>
