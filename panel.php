68d6bb5b5957969b7f8ce1a7ffefc8e79de1ea5f
DrError <daniss404@gmx.com>
2023-04-28 11:48:44 +0300

    quick panel page

---
+++ panel.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>Konsultacijas</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Contact.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 5.6.13, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+
+    
+    <?php 
+include  ('header.php');
+?>
+    <section class="u-clearfix u-section-1" id="sec-b971">
+      <div class="u-clearfix u-sheet u-sheet-1">
+
+
+
+
+
+
+      </div>
+    </section>
+    
+    <?php 
+include ('footer.php');
+?>
+
+  
+</body></html>
