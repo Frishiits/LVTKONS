b6ea0eec53b5a496133649cf457662262562e85e
Frishiits <Frishiitsofc@gmail.com>
2023-04-28 15:18:54 +0300

    samainits

--- panel.php
+++ panel.php
@@ -37,10 +37,10 @@ include  ('header.php');
                         <div id="credit-card" class="tab-pane fade show active pt-3">
                             <form role="form" onsubmit="event.preventDefault()">
                                 <div class="form-group"> <label for="username">
-                                        <h6>Card Owner</h6>
+                                        <h6>Vārds uzvārds</h6>
                                     </label> <input type="text" name="username" placeholder="Card Owner Name" required class="form-control "> </div>
                                 <div class="form-group"> <label for="cardNumber">
-                                        <h6>Card number</h6>
+                                        <h6>Konsultacijas nosaukums</h6>
                                     </label>
                                     <div class="input-group"> <input type="text" name="cardNumber" placeholder="Valid card number" class="form-control " required>
                                         <div class="input-group-append"> <span class="input-group-text text-muted"> <i class="fab fa-cc-visa mx-1"></i> <i class="fab fa-cc-mastercard mx-1"></i> <i class="fab fa-cc-amex mx-1"></i> </span> </div>
