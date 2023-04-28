af66615a39462805ef62aa2126d9a99935fab831
Frishiits <Frishiitsofc@gmail.com>
2023-04-28 15:22:00 +0300

    vel

--- panel.php
+++ panel.php
@@ -38,11 +38,11 @@ include  ('header.php');
                             <form role="form" onsubmit="event.preventDefault()">
                                 <div class="form-group"> <label for="username">
                                         <h6>Vārds uzvārds</h6>
-                                    </label> <input type="text" name="username" placeholder="Card Owner Name" required class="form-control "> </div>
+                                    </label> <input type="text" name="username" placeholder="Vārds uzvārds" required class="form-control "> </div>
                                 <div class="form-group"> <label for="cardNumber">
                                         <h6>Konsultacijas nosaukums</h6>
                                     </label>
-                                    <div class="input-group"> <input type="text" name="cardNumber" placeholder="Valid card number" class="form-control " required>
+                                    <div class="input-group"> <input type="text" name="cardNumber" placeholder="Nosaukums" class="form-control " required>
                                         <div class="input-group-append"> <span class="input-group-text text-muted"> <i class="fab fa-cc-visa mx-1"></i> <i class="fab fa-cc-mastercard mx-1"></i> <i class="fab fa-cc-amex mx-1"></i> </span> </div>
                                     </div>
                                 </div>
