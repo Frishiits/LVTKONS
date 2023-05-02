6080bae7e61830ca42def727b4df99b8db9e1cb1
DrError <daniss404@gmx.com>
2023-05-02 14:01:42 +0300

    undeeded

--- Contact.php
+++ Contact.php
@@ -13,12 +13,6 @@
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
     
     
-    <script type="application/ld+json">{
-		"@context": "http://schema.org",
-		"@type": "Organization",
-		"name": "",
-		"logo": "images/logo_new_balts.png"
-}</script>
     <meta name="theme-color" content="#478ac9">
     <meta property="og:title" content="Contact">
     <meta property="og:type" content="website">
