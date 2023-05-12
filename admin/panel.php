a94504ca36da92a6f0bc03bb61429bf19d14d9c7
DrError <daniss404@gmx.com>
2023-05-12 04:15:35 +0300

    admin panel with css

--- admin/panel.php
+++ admin/panel.php
@@ -2,42 +2,6 @@
 <html style="font-size: 16px;" lang="en">
 
 <head>
-    <style>
-         .navbar {
-     background: #000000
- }
-
- .nav-item::after {
-     content: '';
-     display: block;
-     width: 0px;
-     height: 2px;
-     background: #fec400;
-     transition: 0.4s
- }
-
- .nav-item:hover::after {
-     width: 100%
- }
-
- .navbar-dark .navbar-nav .active>.nav-link,
- .navbar-dark .navbar-nav .nav-link.active,
- .navbar-dark .navbar-nav .nav-link.show,
- .navbar-dark .navbar-nav .show>.nav-link,
- .navbar-dark .navbar-nav .nav-link:focus,
- .navbar-dark .navbar-nav .nav-link:hover {
-     color: #fec400
- }
-
- .nav-link {
-     padding: 25px 5px;
-     transition: 0.2s
- }
- 
- body{
-     background-color:grey;
- }
-        </style>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="utf-8">
   <meta name="keywords" content="">
@@ -45,76 +9,174 @@
   <title></title>
   <link rel="stylesheet" href="../nicepage.css" media="screen">
   <meta name="generator" content="Nicepage 5.6.13, nicepage.com">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <link rel="stylesheet" href="admin.css" media="screen">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.1/css/all.min.css" rel="stylesheet">z
+
   <link id="u-theme-google-font" rel="stylesheet"
     href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
 </head>
 
-<body class="u-body u-xl-mode" data-lang="en">
-        
-
-  <section class="u-align-center u-clearfix u-section-1" id="sec-3165">
-    <div class="u-clearfix u-sheet u-sheet-1">
-      <div class="u-expanded-width u-table u-table-responsive u-table-1">
-        <table class="u-table-entity u-table-entity-1">
-          <colgroup>
-            <col width="20%">
-            <col width="20%">
-            <col width="20%">
-            <col width="20%">
-            <col width="20%">
-          </colgroup>
-          <tbody class="u-table-alt-palette-1-light-3 u-table-body">
-            <tr style="height: 45px;">
-              <td class="u-table-cell">Pirmdiena</td>
-              <td class="u-table-cell">Otrdiena</td>
-              <td class="u-table-cell">Trešdiena</td>
-              <td class="u-table-cell">Ceturtdiena</td>
-              <td class="u-table-cell">Piektdiena</td>
-            </tr>
-            <tr style="height: 65px;">
-              <td class="u-table-cell">Row 1</td>
-              <td class="u-table-cell"></td>
-              <td class="u-table-cell">Description</td>
-              <td class="u-table-cell">Description</td>
-              <td class="u-table-cell">Description</td>
-            </tr>
-            <tr style="height: 65px;">
-              <td class="u-table-cell">Row 2</td>
-              <td class="u-table-cell"></td>
-              <td class="u-table-cell">Description</td>
-              <td class="u-table-cell">Description</td>
-              <td class="u-table-cell">Description</td>
-            </tr>
-            <tr style="height: 65px;">
-              <td class="u-table-cell">Row 3</td>
-              <td class="u-table-cell"></td>
-              <td class="u-table-cell">Description</td>
-              <td class="u-table-cell">Description</td>
-              <td class="u-table-cell">Description</td>
-            </tr>
-            <tr style="height: 65px;">
-              <td class="u-table-cell">Row 4</td>
-              <td class="u-table-cell"></td>
-              <td class="u-table-cell">Description</td>
-              <td class="u-table-cell">Description</td>
-              <td class="u-table-cell">Description</td>
-            </tr>
-          </tbody>
-        </table>
+<body>
+  <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidenav-main">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand pt-0 text-primary" href="https://www.creative-tim.com/product/argon-dashboard" target="_blank">
+        LVT
+      </a>
+      <ul class="nav align-items-center d-md-none">
+        <li class="nav-item dropdown">
+          <a class="nav-link nav-link-icon" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="ni ni-bell-55"></i>
+          </a>
+          <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right" aria-labelledby="navbar-default_dropdown_1">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <div class="media align-items-center">
+              <span class="avatar avatar-sm rounded-circle">
+                <img alt="Image placeholder" src="https://demos.creative-tim.com/argon-dashboard/assets-old/img/theme/team-4.jpg">
+              </span>
+            </div>
+          </a>
+          <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
+            <div class=" dropdown-header noti-title">
+              <h6 class="text-overflow m-0">Welcome!</h6>
+            </div>
+            <a href="./examples/profile.html" class="dropdown-item">
+              <i class="ni ni-single-02"></i>
+              <span>My profile</span>
+            </a>
+            <a href="./examples/profile.html" class="dropdown-item">
+              <i class="ni ni-settings-gear-65"></i>
+              <span>Settings</span>
+            </a>
+            <a href="./examples/profile.html" class="dropdown-item">
+              <i class="ni ni-calendar-grid-58"></i>
+              <span>Activity</span>
+            </a>
+            <a href="./examples/profile.html" class="dropdown-item">
+              <i class="ni ni-support-16"></i>
+              <span>Support</span>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#!" class="dropdown-item">
+              <i class="ni ni-user-run"></i>
+              <span>Logout</span>
+            </a>
+          </div>
+        </li>
+      </ul>
+      <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+        <div class="navbar-collapse-header d-md-none">
+          <div class="row">
+            <div class="col-6 collapse-brand">
+              <a href="javascript:void(0)">
+                Creative Tim
+              </a>
+            </div>
+            <div class="col-6 collapse-close">
+              <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <form class="mt-4 mb-3 d-md-none">
+          <div class="input-group input-group-rounded input-group-merge">
+            <input type="search" class="form-control form-control-rounded form-control-prepended" placeholder="Search" aria-label="Search">
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <span class="fa fa-search"></span>
+              </div>
+            </div>
+          </div>
+        </form>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)">
+              <i class="ni ni-tv-2 text-primary"></i> Dashboard
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
-  </section>
-
-
-  <footer class="u-align-center u-clearfix u-custom-color-2 u-footer u-footer" id="sec-b083">
-    <div class="u-clearfix u-sheet u-sheet-1">
-      <p class="u-small-text u-text u-text-variant u-text-1"> Liepājas Valsts tehnikums © 2023</p>
+  </nav>
+  <div class="main-content">
+    <!-- Top navbar -->
+    <nav class="navbar navbar-tp navbar-expand-md navbar-dark bg-primary" id="navbar-main">
+      <div class="container-fluid">
+        <!-- Brand -->
+        <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="./index.html">Dashboard</a>
+        <!-- Form -->
+        <form class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
+          <div class="form-group mb-0">
+            <div class="input-group input-group-alternative">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fas fa-search"></i></span>
+              </div>
+              <input class="form-control" placeholder="Search" type="text">
+            </div>
+          </div>
+        </form>
+        <!-- User -->
+        <ul class="navbar-nav align-items-center d-none d-md-flex">
+          <li class="nav-item dropdown">
+            <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <div class="media align-items-center">
+                <span class="avatar avatar-sm rounded-circle">
+                  <img alt="Image placeholder" src=" ">
+                </span>
+                <div class="media-body ml-2 d-none d-lg-block">
+                  <span class="mb-0 text-sm  font-weight-bold">Admin Cilveka vards</span>
+                </div>
+              </div>
+            </a>
+            <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
+              <div class=" dropdown-header noti-title">
+                <h6 class="text-overflow m-0">Welcome!</h6>
+              </div>
+              <a href="./examples/profile.html" class="dropdown-item">
+                <i class="ni ni-single-02"></i>
+                <span>My profile</span>
+              </a>
+              <a href="./examples/profile.html" class="dropdown-item">
+                <i class="ni ni-settings-gear-65"></i>
+                <span>Settings</span>
+              </a>
+              <a href="./examples/profile.html" class="dropdown-item">
+                <i class="ni ni-calendar-grid-58"></i>
+                <span>Activity</span>
+              </a>
+              <a href="./examples/profile.html" class="dropdown-item">
+                <i class="ni ni-support-16"></i>
+                <span>Support</span>
+              </a>
+              <div class="dropdown-divider"></div>
+              <a href="#!" class="dropdown-item">
+                <i class="ni ni-user-run"></i>
+                <span>Logout</span>
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container-fluid text-center">
+      <p class="mt-5">Add your content here...</p>
     </div>
-  </footer>
-
+    <footer class="footer" style="margin-top: 70%;">
+      
+    </footer>
+  </div>
 </body>
 
 </html>
