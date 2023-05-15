210ee553736496057c6ec269997d73610b0c1e2f
Dr.Error <56202075+KnsGoyoLV@users.noreply.github.com>
2023-05-15 10:44:34 +0300

    panel changes

--- admin/panel.php
+++ admin/panel.php
@@ -2,7 +2,18 @@
 <html style="font-size: 16px;" lang="lv">
 
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php
+  require("../backend/function.php");
+  MicrosoftInfo();
+  ?>
+
+
+
+
+
+
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="utf-8">
   <meta name="keywords" content="">
   <meta name="description" content="">
@@ -19,13 +30,13 @@
 </head>
 
 <body>
- 
-<div class="main-content">
+
+  <div class="main-content">
     <!-- Top navbar -->
     <nav class="navbar navbar-tp navbar-expand-md navbar-dark bg-primary" id="navbar-main">
       <div class="container-fluid">
         <!-- Brand -->
-        <a class="navbar-brand" href="./index.html">Admin panelis</a>
+        <a class="navbar-brand" href="../index.php">Liepajas Valsts tehnikums</a>
         <!-- Form -->
         <form class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
           <div class="form-group mb-0">
@@ -40,13 +51,14 @@
         <!-- User -->
         <ul class="navbar-nav align-items-center d-none d-md-flex">
           <li class="nav-item dropdown">
-            <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+              aria-expanded="false">
               <div class="media align-items-center">
-                <span class="avatar avatar-sm rounded-circle">
-                  <img alt="Image placeholder" src="">
-                </span>
+
                 <div class="media-body ml-2 d-none d-lg-block">
-                  <span class="mb-0 text-sm font-weight-bold">Admin Cilveka vards</span>
+                  <span class="mb-0 text-sm font-weight-bold">
+                    <?php echo $_SESSION['fullname']; ?>
+                  </span>
                 </div>
               </div>
             </a>
